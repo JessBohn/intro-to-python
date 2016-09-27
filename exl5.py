c27f81a85d58cbb7ef0ea31bb867d7c4895cc95c
Jessica Bohn <bohn.16@wright.edu>
2016-09-26 20:18:11 -0500

    Complete exercise 10 - tabbing lines and intro to escape sequences

--- exl5.py
+++ exl5.py
@@ -48,3 +48,18 @@ Even 4 lines if we want, or 5, or 6.
 """
 
 ## Exercise 10
+
+tabby_cat = "\tI'm tabbed in." # \t creates a tabbed beginning for a line
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
